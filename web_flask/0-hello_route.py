da1b3f5d41b546b69cfa6bd2c7512d69cece8a0d
pblinkprosper <pblinkprosper01@gmail.com>
2024-12-24 18:11:13 +0000

    update on setup files

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,10 +7,10 @@ app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
-def hello_hbnb():
+def index():
     """Displays 'Hello HBNB!'"""
     return "Hello HBNB!"
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port="5000")
