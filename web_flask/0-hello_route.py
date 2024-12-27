b20e0cbc5d54205bf56ce19be2aef9df5a14066e
pblinkprosper <pblinkprosper01@gmail.com>
2024-12-27 17:16:03 +0000

    Update task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,9 +6,9 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
+@app.route("/airbnb-onepage/", strict_slashes=False)
 def index():
-    """Displays 'Hello HBNB!'"""
+    """Displays Hello HBNB!'"""
     return "Hello HBNB!"
 
 
